3af444cf68bf1dd3b07bc2727f4b13ab8c734754
Juandi <juandvaltri@outlook.com>
2024-04-17 22:47:25 -0500

    Commit de clases Persona e inicio de clases Piso y PisoPK.

---
+++ src/main/java/co/edu/unbosque/catastromunicipal/persistence/entity/PisoPK.java
@@ -0,0 +1,11 @@
+package co.edu.unbosque.catastromunicipal.persistence.entity;
+
+import jakarta.persistence.Embeddable;
+
+import java.io.Serializable;
+
+@Embeddable
+public class PisoPK implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+}
