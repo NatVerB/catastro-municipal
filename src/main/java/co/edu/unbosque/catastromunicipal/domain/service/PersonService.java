fa8094c2241d9cbc935e8a15b97f30427a5578a6
NatVerB <natabarrera4@gmail.com>
2024-05-20 03:19:11 -0500

    auxilio

--- src/main/java/co/edu/unbosque/catastromunicipal/domain/service/PersonService.java
+++ src/main/java/co/edu/unbosque/catastromunicipal/domain/service/PersonService.java
@@ -6,22 +6,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PersonService {
     @Autowired
     private PersonRepository personRepository;
 
-    public Person save(Person person){
+    public Person save(Person person) {
         return personRepository.save(person);
     }
-    public List<Person> getAllPersons(){
+
+    public List<Person> getAllPersons() {
         return personRepository.getAllPersons();
     }
+
+    public Optional<Person> getById(Integer id) {
+        return Optional.of(personRepository.getById(id));
     }
+
+    public boolean deleteById(Integer id) {
+        if (getById(id).isPresent()) {
             personRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
         }
     }
+
+    public boolean update(Person person) {
+        if (getById(person.getId()).isPresent()) {
+            personRepository.update(person);
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
--- src/main/java/co/edu/unbosque/catastromunicipal/persistence/crud/BloqueCasasCrudRepository.java
+++ src/main/java/co/edu/unbosque/catastromunicipal/persistence/crud/BloqueCasasCrudRepository.java
@@ -3,7 +3,6 @@ package co.edu.unbosque.catastromunicipal.persistence.crud;
 import co.edu.unbosque.catastromunicipal.persistence.entity.BloqueCasasPK;
 import co.edu.unbosque.catastromunicipal.persistence.entity.BloqueCasas;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 import java.util.List;
 
 public interface BloqueCasasCrudRepository extends CrudRepository<BloqueCasas, BloqueCasasPK> {
